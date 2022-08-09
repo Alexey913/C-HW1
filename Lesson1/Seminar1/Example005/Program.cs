f8931a87d03292e3cb12a799a6499e30a614fa3d
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:26:22 +0300

    Example005 Numbers from -N to N

---
+++ Lesson1/Seminar1/Example005/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+if (N>0)
+{
+    while (count<N)
+    {
+        Console.Write (count);
+        Console.Write (", ");
+        count++;
+    }
+    Console.WriteLine (N);
+}
+else
+{
+  while (count>N)
+    {
+        Console.Write (count);
+        Console.Write (", ");
+        count=count-1;
+    }
+    Console.WriteLine (N);  
+}
