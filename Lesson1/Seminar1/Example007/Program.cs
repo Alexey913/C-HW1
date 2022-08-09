90aeb3caaa9f8d34174831569bc5004ea35eef98
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:29:48 +0300

    Example007 Last digit of 3-discharge number

---
+++ Lesson1/Seminar1/Example007/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N>99&&N<1000||N>-1000&&N<-99)
+{
+int div=Math.Abs(N%10);
+Console.Write("Последняя цифра в числе равна ");
+Console.WriteLine(div);
+}
+else
+{
+Console.WriteLine("Число не является трехзначным");
+}
+
