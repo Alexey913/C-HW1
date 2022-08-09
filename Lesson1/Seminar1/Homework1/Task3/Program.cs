39b2af7391f6654872350342b4259bc5de1eeb09
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:33:22 +0300

    Task3 Even number

---
+++ Lesson1/Seminar1/Homework1/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число, и я определю, чётное оно, или нет: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if (Num%2==0)
+{
+    Console.WriteLine ("Да, число является чётным");
+}
+else
+{ 
+    Console.WriteLine ("Нет, число не является чётным");
+}
