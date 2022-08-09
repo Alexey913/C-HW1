5a6b981812d6028f62ba3d035d66303f36d4e0ca
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:13:37 +0300

    Example001 Square number

---
+++ Lesson1/Seminar1/Example001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqr = number*number;
+Console.Write ("Квадрат числа равен ");
+Console.WriteLine (sqr);
