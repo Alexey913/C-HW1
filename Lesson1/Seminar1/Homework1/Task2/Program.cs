e5ee67d0b64a3fa1563a3adc65c2b6c44cc9defa
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:32:19 +0300

    Task2 Max of 3 number

---
+++ Lesson1/Seminar1/Homework1/Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите три числа, и я определю максимальное");
+Console.Write("Введите первое число: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int Num3 = Convert.ToInt32(Console.ReadLine());
+int max=Num1;
+if (Num2>max)
+{
+    max=Num2;
+}
+if (Num3>max)
+{ 
+    max=Num3;
+}
+Console.Write ("Большим является число: ");
+Console.WriteLine (max);
