7d88a0c99d307ef0042a5e5c1dc926d351079b15
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:17:17 +0300

    Example002 Check square number

---
+++ Lesson1/Seminar1/Example002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b*b==a)
+{
+    Console.WriteLine ("Да ");
+}
+else
+{
+    Console.WriteLine ("Нет ");
+}
