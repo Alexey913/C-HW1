1bd2eec19efd16503330a7120ad571bbb45e4d85
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:03:33 +0300

    Example006 Valera

---
+++ Lesson1/Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("И как же тебя зовут? ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "валера")
+{
+ Console.WriteLine("Йу-ху! Это же Валерончик!");
+}
+else
+{
+  Console.Write("А, это опять ты... Пфф... ");
+  Console.WriteLine(username);
+}
