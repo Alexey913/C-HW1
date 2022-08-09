462d1a421efdd58df6b9d74fc1075929a19feb75
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:20:17 +0300

    Example003 Days of week

---
+++ Lesson1/Seminar1/Example003/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine ("Введите номер дня недели");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a==1)
+{
+    Console.WriteLine ("Понедельник");
+}
+if (a==2)
+{
+    Console.WriteLine ("Вторник");
+}
+if (a==3)
+{
+    Console.WriteLine ("Среда");
+}
+if (a==4)
+{
+    Console.WriteLine ("Четверг");
+}
+if (a==5)
+{
+    Console.WriteLine ("Пятница");
+}
+if (a==6)
+{
+    Console.WriteLine ("Суббота");
+}
+if (a==7)
+{
+    Console.WriteLine ("Воскресенье");
+}
+if (a<1|a>7)
+{
+    Console.WriteLine ("В неделе всего 7 дней. Введите число от 1 до 7");
+}
