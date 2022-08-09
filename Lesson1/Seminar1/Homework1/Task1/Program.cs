63f73f04cc745e02a1604123e9915b8e4b8e22ba
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:31:21 +0300

    Task1 Max and min of 2 numbers

---
+++ Lesson1/Seminar1/Homework1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два числа, и я определю, какое из них большее, а какое меньшее");
+Console.Write("Введите первое число: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Большим является число: ");
+if (Num1>Num2)
+{
+    Console.WriteLine (Num1);
+    Console.Write ("Меньшее, соответственно: ");
+    Console.WriteLine (Num2);
+}
+else
+{
+    Console.WriteLine (Num2);
+        Console.Write ("Меньшее, соответственно: ");
+    Console.WriteLine (Num1);
+}
