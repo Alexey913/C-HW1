fcecc55a9433dbb7ea80c92556264c0d1c4e35c8
Alexey Shevtsov <al913@mail.ru>
2022-08-09 23:34:28 +0300

    Task4 All even numbers from 1 to N

---
+++ Lesson1/Seminar1/Homework1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число N, и я определю все чётные числа от 1 до N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+if (N>0)
+{while (count<=N)
+    {if (count%2==0)
+        {
+        Console.WriteLine (count);
+        }
+        count++;
+    }
+}
+else
+    {
+        N=-N;
+        Console.WriteLine (0);
+        while (count<=N)
+    {if (count%2==0)
+        {
+        Console.WriteLine (-count);
+        }
+        count++;
+    }
+    }
